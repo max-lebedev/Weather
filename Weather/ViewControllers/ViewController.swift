34bc05322f052c604364a13a885103fade67d1ce
Maxim Lebedev <lmax942@gmail.com>
2021-09-04 15:19:11 +0300

    Updating interface

--- Weather/ViewControllers/ViewController.swift
+++ Weather/ViewControllers/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
             self.networkWeatherManager.fetchCurrentWeather(forCity: city)
         }
     }
@@ -26,7 +26,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        networkWeatherManager.fetchCurrentWeather(forCity: "Moscow")
+        networkWeatherManager.onCompletion = { [weak self] currentWeather in
+            guard let self = self else { return }
+            self.updateInterfaceWith(weather: currentWeather)
+        }
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+    }
+    
+    func updateInterfaceWith(weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString + " °C"
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+        }
     }
 }
-
